d7b8abe816e5db0e175a58994189390e17b3415d
yoshmello <yoshmello@gmail.com>
2016-05-18 17:13:49 -0400

    VendingMachine
    C++ Object Oriented Design Vending Machine Operation

---
+++ COPVending/COPVending/COPVending/COPVending.cpp
@@ -0,0 +1,19 @@
+// COPVending.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "Vending.h"
+
+
+using namespace std;
+
+int main()
+{
+	Vending test;
+	
+
+
+	test.machineRun();
+
+}
+
